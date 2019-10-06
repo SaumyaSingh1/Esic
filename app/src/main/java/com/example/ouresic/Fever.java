b2eb00975c336acc66c0986e7c710999294d6c72
Saumya <singhsaumyas150@gmail.com>
2019-10-06 19:03:45 +0530

    Print Feature Enabled and UI updated

--- app/src/main/java/com/example/ouresic/Fever.java
+++ app/src/main/java/com/example/ouresic/Fever.java
@@ -1,15 +1,29 @@
 package com.example.ouresic;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
 
 public class Fever  extends AppCompatActivity  //prescription for fever
 
 {
+
+    Button btnPrint;
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.fever);
+
+        btnPrint = findViewById(R.id.btnPrint);
+        btnPrint.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                startActivity(new Intent(getBaseContext(), entry.class));
+            }
+        });
     }
 }
